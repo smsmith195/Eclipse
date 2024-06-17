782df49a61fd7f9695c1b8ffa307d693d3c8fce9
Stephen Mark Smith <smsmith195@gmail.com>
2024-06-17 17:25:05 +0100

    Create Enemy AI and Pathfinding

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum State
     {
-        
+        Roaming
     }
 
-    // Update is called once per frame
-    void Update()
+    private State state;
+    private EnemyPathfinding enemyPathfinding;
+
+    private void Awake()
     {
-        
+        enemyPathfinding = GetComponent<EnemyPathfinding>();
+        state = State.Roaming;
     }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            Vector2 roamPosition = GetRoamingPosition();
+            enemyPathfinding.MoveTo(roamPosition);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+
 }
