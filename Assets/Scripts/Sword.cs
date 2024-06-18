88136a009716bdd4480d909db077f5c219eaae17
Stephen Mark Smith <smsmith195@gmail.com>
2024-06-18 16:55:56 +0100

    Add sword sprite flipping

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,11 +6,15 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake()
     {
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControls();
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
     }
 
     private void OnEnable()
@@ -23,8 +27,30 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
