921a9f0cd490cb17805fd6c5874f19a53ab981fb
Stephen Mark Smith <smsmith195@gmail.com>
2024-06-19 13:47:40 +0100

    Sword Collision Added

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,6 +6,7 @@ public class Sword : MonoBehaviour
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
+    [SerializeField] private Transform weaponCollider;
     
     private PlayerControls playerControls;
     private Animator myAnimator;
@@ -41,8 +42,15 @@ public class Sword : MonoBehaviour
     {
         myAnimator.SetTrigger("Attack");
 
+        weaponCollider.gameObject.SetActive(true);
+
         slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
         slashAnim.transform.parent = this.transform.parent;
+    }
+
+    public void DoneAttackingAnimEvent()
+    {
+        weaponCollider.gameObject.SetActive(false);
     }
 
     public void SwingUpFlipAnim()
@@ -73,10 +81,12 @@ public class Sword : MonoBehaviour
         if (mousePos.x < playerScreenPoint.x)
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
         }
         else
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
