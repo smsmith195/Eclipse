1879d56c4822534b1bb9bee52d10923adb977d34
Stephen Mark Smith <smsmith195@gmail.com>
2024-09-14 14:12:27 +0100

    Fades and Portal VFX

--- Assets/Scripts/Scene Management/AreaExit.cs
+++ Assets/Scripts/Scene Management/AreaExit.cs
@@ -8,13 +8,25 @@ public class AreaExit : MonoBehaviour
     [SerializeField] private string sceneToLoad;
     [SerializeField] private string sceneTransitionName;
 
+    private float waitToLoadTime = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
             SceneManagement.Instance.SetTransistionName(sceneTransitionName);
+            UI_Fade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
         }
+    }
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        while (waitToLoadTime >= 0)
+        {
+            waitToLoadTime -= Time.deltaTime;
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
