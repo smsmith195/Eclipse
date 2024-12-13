dbe524ceeb2842398cc381a2591729021ed47220
Stephen Smith <51889637+smsmith195@users.noreply.github.com>
2024-12-13 19:51:40 +0000

    Revert "Player Health initial rigging"
    This reverts commit 6804d2d8a6c339b24a93e2b4702fe0f1eeb09ddc.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -16,7 +16,6 @@ public class PlayerController : Singleton<PlayerController>
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRender;
-    private Knockback knockback;
     private float startingMoveSpeed;
 
     private bool facingLeft = false;
@@ -30,7 +29,6 @@ public class PlayerController : Singleton<PlayerController>
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
-        knockback = GetComponent<Knockback>();
     }
 
     private void Start()
@@ -69,7 +67,6 @@ public class PlayerController : Singleton<PlayerController>
 
     private void Move()
     {
-        if (knockback.GettingKnockBack) { return; }
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
