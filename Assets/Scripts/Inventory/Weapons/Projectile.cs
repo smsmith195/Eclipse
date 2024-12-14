0b56684d473a089f448fe911d50c4b26dc13e915
Stephen Smith <51889637+smsmith195@users.noreply.github.com>
2024-12-14 15:29:02 +0000

    Bullet/Projectile Refactor

--- Assets/Scripts/Inventory/Weapons/Projectile.cs
+++ Assets/Scripts/Inventory/Weapons/Projectile.cs
@@ -7,8 +7,9 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 22f;
     [SerializeField] private GameObject particleOnHitPrefabVFX;
-
-    private WeaponInfo weaponInfo;
+    [SerializeField] private bool isEnemyProjectile = false;
+    [SerializeField] private float projectileRange = 10f;
+    
     private Vector3 startPosition;
 
     private void Start()
@@ -22,9 +23,9 @@ public class Projectile : MonoBehaviour
         DetectFireDistance();
     }
 
-    public void UpdateWeaponInfo(WeaponInfo weaponInfo)
+    public void UpdateProjectileRange(float projectileRange)
     {
-        this.weaponInfo = weaponInfo;
+        this.projectileRange = projectileRange;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -32,8 +33,15 @@ public class Projectile : MonoBehaviour
         EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
         Indestructible indestructible = other.gameObject.GetComponent<Indestructible>();
 
-        if (!other.isTrigger && (enemyHealth || indestructible))
+        PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
+
+        if (!other.isTrigger && (enemyHealth || indestructible || player))
         {
+            if (player && isEnemyProjectile)
+            {
+                player.TakeDamage(1, transform);
+            }
+
             Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
             Destroy(gameObject);
         }  
@@ -41,7 +49,7 @@ public class Projectile : MonoBehaviour
 
     private void DetectFireDistance()
     {
-        if (Vector3.Distance(transform.position, startPosition) > weaponInfo.weaponRange)
+        if (Vector3.Distance(transform.position, startPosition) > projectileRange)
         {
             Destroy(gameObject);
         }
