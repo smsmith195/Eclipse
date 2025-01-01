9e458282a435821c1fbbea5e281fc5ff64c58e81
Stephen Smith <51889637+smsmith195@users.noreply.github.com>
2025-01-01 13:51:40 +0000

    Bomb projectile animations created

---
+++ Assets/Scripts/Enemy/BombProjectile.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombProjectile : MonoBehaviour
+{
+    [SerializeField] private float duration = 1f;
+    [SerializeField] private AnimationCurve bombAnimCurve;
+    [SerializeField] private float heightY = 3f;
+    [SerializeField] private GameObject projectileShadow;
+    [SerializeField]private Vector3 shadowSpawnPos;
+    
+    private void Start()
+    {
+        GameObject shadow = Instantiate(projectileShadow, transform.position + shadowSpawnPos, Quaternion.identity);
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+        Vector3 shadowStartPos = projectileShadow.transform.position;
+        
+        StartCoroutine(ProjectileCurveRoutine(transform.position, playerPos));
+        StartCoroutine(MoveShadowRoutine(shadow, shadowStartPos, playerPos));
+    }
+
+    private IEnumerator ProjectileCurveRoutine(Vector3 startPosition, Vector3 endPosition)
+    {
+        float timePassed = 0f;
+
+        while (timePassed < duration)
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / duration;
+            float heightT = bombAnimCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPosition, endPosition, linearT) + new Vector2(0f, height);
+
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+
+    private IEnumerator MoveShadowRoutine(GameObject shadow, Vector3 startPosition, Vector3 endPosition)
+    {
+        float timePassed = 0f;
+
+        while(timePassed < duration)
+        {
+            timePassed += Time.deltaTime;
+            
+            float linearT = timePassed / duration;
+
+            shadow.transform.position = Vector2.Lerp(startPosition, endPosition, linearT);
+
+            yield return null;
+        }
+        Destroy(shadow);
+    }
+
+}
