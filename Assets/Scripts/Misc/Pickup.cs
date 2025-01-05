96809da8e1355a08b1175724d1ea2bd0705d5eb2
Stephen Smith <51889637+smsmith195@users.noreply.github.com>
2025-01-05 15:13:47 +0000

    Coin animation

--- Assets/Scripts/Misc/Pickup.cs
+++ Assets/Scripts/Misc/Pickup.cs
@@ -8,6 +8,9 @@ public class Pickups : MonoBehaviour
     [SerializeField] private float pickUpDistance = 5f;
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float accelerationRate = .2f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
 
     private Vector3 moveDir;
     private Rigidbody2D rb;
@@ -15,6 +18,11 @@ public class Pickups : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(AnimCurveSpawnRoutine());
     }
 
     private void Update()
@@ -43,6 +51,28 @@ public class Pickups : MonoBehaviour
         if (other.gameObject.GetComponent<PlayerController>())
         {
             Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator AnimCurveSpawnRoutine()
+    {
+        Vector2 startPoint = transform.position;
+        float randomX = transform.position.x + Random.Range(-2f, 2f);
+        float randomY = transform.position.y + Random.Range(-1f, 1f);
+
+        Vector2 endPoint = new Vector2(randomX, randomY);
+
+        float timePassed = 0f;
+
+        while (timePassed < popDuration)
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPoint, endPoint, linearT) + new Vector2(0f, height);
+            yield return null;
         }
     }
 }
