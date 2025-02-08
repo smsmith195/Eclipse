7f3578364a3cacda1dba37a9bb7110a56864ecb1
Stephen Smith <51889637+smsmith195@users.noreply.github.com>
2025-02-08 15:03:01 +0000

    Cursor Implementation

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CursorManager : MonoBehaviour
+{
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    
+    void Start()
+    {
+        Cursor.visible = false;
+
+        if (Application.isPlaying)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Input.mousePosition;
+        image.rectTransform.position = cursorPos;
+
+        /*if(!Application.isPlaying)
+        {
+            return;
+        }
+
+        Cursor.visible = false;*/
+    }
+}
